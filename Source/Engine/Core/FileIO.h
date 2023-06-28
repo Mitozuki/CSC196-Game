80c6a66b9dd559c215f1669ca17b3ccc12091b62
Mitozuki <129234368+Mitozuki@users.noreply.github.com>
2023-06-28 14:44:50 -0600

    Day 3

--- Source/Engine/Core/FileIO.h
+++ Source/Engine/Core/FileIO.h
@@ -6,4 +6,9 @@
 namespace kiko
 {
 	std::string getFilePath();
+	bool setFilePath(const std::filesystem::path& path);
+	bool FileExists(const std::filesystem::path& path);
+	bool getFileSize(const std::filesystem::path& path, size_t& size);
+	bool readFile(const std::filesystem::path& path, std::string& buffer);
+
 }
--- Source/Game/Game/Main.cpp
+++ Source/Game/Game/Main.cpp
@@ -1,12 +1,26 @@
 #include "Core/Random.h"
 #include "Core/FileIO.h"
+#include "Core/Memory.h";
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
+	int* p = new int;
+	delete p;
+
 	cout << kiko::getFilePath() << endl;
+	kiko::setFilePath("Assets");
+	cout << kiko::getFilePath() << endl;
+	size_t size;
+	kiko::getFileSize("game.txt", size);
+	cout << size << endl;
+
+	std::string s;
+	kiko::readFile("game.txt", s);
+	cout << s << endl;
+
 	kiko::seed_random((unsigned int)time(nullptr));
 	for (int i = 0; i < 10; i++)
 	{
