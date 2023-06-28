3e007988acdbd573f90b9d6033d5be4ba73a3185
Mitozuki <129234368+Mitozuki@users.noreply.github.com>
2023-06-28 13:36:07 -0600

    Day 2 Changes

--- Source/Game/Game/Main.cpp
+++ Source/Game/Game/Main.cpp
@@ -1,9 +1,15 @@
+#include "Core/Random.h"
+#include "Core/FileIO.h"
 #include <iostream>
+
+using namespace std;
 
 int main()
 {
-#ifdef _DEBUG
-	std::cout << "debug!\n";
-#endif
-	std::cout << "hello world!\n";
+	cout << kiko::getFilePath() << endl;
+	kiko::seed_random((unsigned int)time(nullptr));
+	for (int i = 0; i < 10; i++)
+	{
+		cout << kiko::random(5, 10) << endl;
+	}
 }
