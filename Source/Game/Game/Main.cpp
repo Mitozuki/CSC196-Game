e6736251e96a94a5d2137cab701f633a546f5cf2
Mitozuki <129234368+Mitozuki@users.noreply.github.com>
2023-06-29 14:37:57 -0600

    Day 4

--- Source/Game/Game/Main.cpp
+++ Source/Game/Game/Main.cpp
@@ -1,16 +1,25 @@
 #include "Core/Random.h"
 #include "Core/FileIO.h"
-#include "Core/Memory.h";
+#include "Core/Memory.h"
+#include "Core/Time.h"
+#include "Renderer/Renderer.h"
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
+	kiko::g_memoryTracker.DisplayInfo();
 	int* p = new int;
+	kiko::g_memoryTracker.DisplayInfo();
 	delete p;
+	kiko::g_memoryTracker.DisplayInfo();
 
-	cout << kiko::getFilePath() << endl;
+	kiko::Time timer;
+	for (int i = 0; i < 100000; i++) {}
+	cout << timer.GetElapsedSeconds() << endl;
+
+	/*cout << kiko::getFilePath() << endl;
 	kiko::setFilePath("Assets");
 	cout << kiko::getFilePath() << endl;
 	size_t size;
@@ -25,5 +34,5 @@ int main()
 	for (int i = 0; i < 10; i++)
 	{
 		cout << kiko::random(5, 10) << endl;
-	}
+	}*/
 }
