d8efd1db945f547702ebdbbbf03d5671a9d55c46
Mitozuki <129234368+Mitozuki@users.noreply.github.com>
2023-07-11 14:43:33 -0600

    Asset Load

--- Source/Game/Game/Main.cpp
+++ Source/Game/Game/Main.cpp
@@ -4,6 +4,7 @@
 #include "Input/InputSystem.h"
 #include <iostream>
 #include <vector>
+#include <thread>
 
 using namespace std;
 
@@ -14,7 +15,7 @@ public:
 
 	void Update(int width, int height)
 	{
-		m_pos += m_vel;
+		m_pos += m_vel * kiko::g_time.GetDeltaTime();
 		if (m_pos.x >= width) m_pos.x = 0;
 		if (m_pos.y >= height) m_pos.y = 0;
 	}
@@ -32,6 +33,7 @@ public:
 int main(int argc, char* argv[])
 {
 	kiko::seed_random((unsigned int)time(nullptr));
+	kiko::setFilePath("Assets");
 
 	kiko::Renderer renderer;
 	renderer.Initialize();
@@ -41,8 +43,9 @@ int main(int argc, char* argv[])
 	inputSystem.Initialize();
 
 
-	std::vector<kiko::vec2> points{ { 10, 0 }, { 5, 5 }, { 0, -5 }, { 10, 0 } };
-	kiko::Model model{ points };
+	//std::vector<kiko::vec2> points{ { 10, 0 }, { 5, 5 }, { 0, -5 }, { 10, 0 } };
+	kiko::Model model;
+	model.Load("star.txt");
 
 	kiko::vec2 v{5, 5};
 	v.Normalize();
@@ -56,15 +59,29 @@ int main(int argc, char* argv[])
 		stars.push_back(Star(pos, vel));
 	}
 
+	kiko::vec2 position{ 400, 300 };
+	float speed = 100;
+
+	// main game loop
 	bool quit = false;
 	while (!quit)
 	{
+		kiko::g_time.Tick();
 		inputSystem.Update();
 		cout << inputSystem.GetMousePosition().x << " " << inputSystem.GetMousePosition().y << endl;
 		if (inputSystem.GetKeyDown(SDL_SCANCODE_ESCAPE))
 		{
 			quit = true;
 		}
+
+		kiko::vec2 direction;
+		if (inputSystem.GetKeyDown(SDL_SCANCODE_W)) direction.y = -1;
+		if(inputSystem.GetKeyDown(SDL_SCANCODE_A)) direction.x = -1;
+		if(inputSystem.GetKeyDown(SDL_SCANCODE_S)) direction.y = 1;
+		if(inputSystem.GetKeyDown(SDL_SCANCODE_D)) direction.x = 1;
+
+		position += direction * speed * kiko::g_time.GetDeltaTime();
+
 		if (inputSystem.GetMouseButtonDown(0))
 		{
 			cout << "left mouse pressed " << endl;
@@ -91,9 +108,11 @@ int main(int argc, char* argv[])
 			renderer.DrawPoint(star.m_pos.x, star.m_pos.y);
 		}
 
-		model.Draw(renderer, { 400, 300 }, 3);
+		model.Draw(renderer, position, 3);
 
 		renderer.EndFrame();
+
+		//this_thread::sleep_for(chrono::milliseconds(100));
 	}
 
 	return 0;
